41b72b04fd420b5a65d3786c86d92ebff2a3a386
rom1313 <romain.gavin@outlook.fr>
2022-08-28 11:35:26 +0200

    Added my project

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
+// todo -------------- Création d'un Génius ---------------------------------
+
 const genius = $
 
-style = 'color:orange;position:absolute;top:90%;left:90%;transform: translate(-50%, -50%);font-size:40px'
+// todo -------------- Création d'un style ----------------------------------
 
-let p = genius.element('h1','#test',style)
-let boutton = genius.element('button','#bouttontest')
- let p2 =genius.element('p','#text')
-genius.textContent(genius.heure(),'#test')
-genius.textContent('Boutton','#bouttontest')
+let style = 'color:orange;position:absolute;top:90%;left:90%;transform: translate(-50%, -50%);font-size:40px'
 
-genius.event('#bouttontest','click',(e)=>{
-console.log('boutton appuyé !');
-p.classList.add('heureanimation')
+// todo -------------- Création d'éléments HTML -----------------------------
 
+let p = genius.element('h1', '#test', style)
+let boutton = genius.element('button', '#bouttontest')
+
+// todo -------------- Modification du textContent des éléments HTML --------
+
+genius.textContent('Clique-moi', '#bouttontest')
+
+// todo -------------- Création d'évènements --------------------------------
+// event boutton
+genius.event('#bouttontest', 'click', (e) => {
+    console.log('boutton appuyé !');
+    p.classList.add('heureanimation')
 })
-genius.event('#input','keyup',(e)=>{
+// event input
+genius.event('#input', 'keyup', (e) => {
     let text = e.target.value
-   
-    p2.textContent = text
-    })
+    genius.textContent(text, '#text')
+})
+
+// todo -------------- Heure dynamique ---------------------------------------
+//on créer la fonction 
+function heureDynamique() {
+    genius.textContent(genius.heure(), '#test')
+    requestAnimationFrame(heureDynamique)
+}
+
+//on appel "l'animation"
+requestAnimationFrame(heureDynamique) 
