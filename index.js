3b0ba10cc6d5a67681256249d97c3a246307b1aa
rom1313 <romain.gavin@outlook.fr>
2022-08-28 10:22:08 +0200

    Added my project

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ style = 'color:orange;position:absolute;top:90%;left:90%;transform: translate(-5
 
 let p = genius.element('h1','#test',style)
 let boutton = genius.element('button','#bouttontest')
-
+ let p2 =genius.element('p','#text')
 genius.textContent(genius.heure(),'#test')
 genius.textContent('Boutton','#bouttontest')
 
@@ -13,3 +13,8 @@ console.log('boutton appuyé !');
 p.classList.add('heureanimation')
 
 })
+genius.event('#input','keyup',(e)=>{
+    let text = e.target.value
+   
+    p2.textContent = text
+    })
